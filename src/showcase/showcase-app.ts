2b76f2e7712f53388163d38a9f48843aa2e89ab3
Andreas Weber <weber.andreas@gmail.com>
2024-08-14 21:58:16 +0200

    feat: add source link

--- src/showcase/showcase-app.ts
+++ src/showcase/showcase-app.ts
@@ -8,6 +8,10 @@ export class ShowcaseApp extends LitElement {
   public override render() {
     return html`<div class="buttongroup">
       <button @click=${() => this.setFeatures(locations)}>Heatmap</button>
+      <a
+        href="https://github.com/AnWeber/lit-maplibre/blob/main/src/components/map-viewer.ts#L71"
+        >Source</a
+      >
     </div>`;
   }
 
